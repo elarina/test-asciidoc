924554dd1ab3636959f350270eae645eb6a8f05b
Elena Larina <Elena.Larina@advalange.com>
2025-09-29 16:51:43 +0400

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const { src, dest } = require('gulp');
+const fileinclude = require('gulp-file-include');
+const asciidoctor = require('@asciidoctor/core')();
+
+function build() {
+	convertAdoc();
+	return includeHtml();
+}
+
+// Экспорт задачи
+exports.build = build;
+
+// Задача для вставки содержимого одного HTML-файла в другой
+function includeHtml() {
+  return src('content1.html')
+    .pipe(fileinclude({
+      prefix: '@@', // Префикс для включения
+      basepath: '@file' // Базовый путь для включений
+    }))
+    .pipe(dest('./dest'));
+}
+
+// Экспорт задачи
+exports.includeHtml = includeHtml;
+
+function convertAdoc() {
+  return asciidoctor.convertFile('document.adoc', {
+    to_file: 'gen-tz1.html', 
+	standalone: false,
+	safe: 'safe'
+  });
+}
+
+// Экспорт задачи
+exports.convertAdoc = convertAdoc;
